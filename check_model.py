a51b6a79218afab0d63c5b2af9b0c9caa7283407
amanwork5454-debug <amanwork5454@gmail.com>
2026-03-28 11:53:03 +0530

    Remove unnecessary files

--- check_model.py
+++
@@ -1,8 +0,0 @@
-import pickle
-with open('model.pkl', 'rb') as f:
-    m = pickle.load(f)
-print(type(m))
-if isinstance(m, dict):
-    print("Keys:", m.keys())
-else:
-    print("Not a dict - old format!")
